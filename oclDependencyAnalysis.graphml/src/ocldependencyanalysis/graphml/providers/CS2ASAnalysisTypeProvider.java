58d5f2ca6e4c0cacf7dc109d9e8aceff4539089e
Adolfo SBH <adolfosbh@gmail.com>
2014-09-02 17:16:37 +0100

    - Minor bug fix - Printing some identifying colours to the actions

--- oclDependencyAnalysis.graphml/src/ocldependencyanalysis/graphml/providers/CS2ASAnalysisTypeProvider.java
+++ oclDependencyAnalysis.graphml/src/ocldependencyanalysis/graphml/providers/CS2ASAnalysisTypeProvider.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ocldependencyanalysis.NameResoPropertyObj;
 import ocldependencyanalysis.cs2asanalysis.ActionNode;
 import ocldependencyanalysis.cs2asanalysis.CS2ASAnalysisNode;
-import ocldependencyanalysis.cs2asanalysis.ExtendedPropertyInfo;
+import ocldependencyanalysis.cs2asanalysis.ConstructorPartAction;
 import ocldependencyanalysis.cs2asanalysis.InfoNode;
+import ocldependencyanalysis.cs2asanalysis.OperationAction;
+import ocldependencyanalysis.cs2asanalysis.PropertyRef;
 import ocldependencyanalysis.graph.IEdge;
 import ocldependencyanalysis.graph.IGraph;
 import ocldependencyanalysis.graph.INode;
@@ -16,11 +19,12 @@ import ocldependencyanalysis.graphml.IElementTypeProvider;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ocl.examples.pivot.Element;
+import org.eclipse.ocl.examples.pivot.Operation;
 import org.eclipse.ocl.examples.pivot.Package;
+import org.eclipse.ocl.examples.pivot.Property;
 import org.eclipse.qvtd.build.etl.graph.EdgeType;
 import org.eclipse.qvtd.build.etl.graph.ElementType;
 import org.eclipse.qvtd.build.etl.graph.NodeType;
-import org.eclipse.qvtd.build.etl.graphmltypes.EdgeEndStyle;
 import org.eclipse.qvtd.build.etl.graphmltypes.EdgeLineStyle;
 import org.eclipse.qvtd.build.etl.graphmltypes.GraphMLEdgeType;
 import org.eclipse.qvtd.build.etl.graphmltypes.GraphMLNodeType;
@@ -41,12 +45,20 @@ public class CS2ASAnalysisTypeProvider implements  IElementTypeProvider<CS2ASAna
 	//99CCFF - Blue (output)
 	
 	private List<ElementType> elementTypes;	
-	private GraphMLNodeType actionNode;
+	private GraphMLNodeType cstElementActionNode;
+	private GraphMLNodeType astElementActionNode;
+	
+	private GraphMLNodeType attributePropertyActionNode;
+	private GraphMLNodeType containmentPropertyActionNode;
+	private GraphMLNodeType nonContainmentPropertyActionNode;
+
+	private GraphMLNodeType containmentNameResoPropertyActionNode;
+	private GraphMLNodeType nonContainmentNameResoPropertyActionNode;
 	
 	private GraphMLEdgeType actionInput;
 	private GraphMLEdgeType actionOutput;
-	private GraphMLEdgeType infoInheritance;
-	private GraphMLEdgeType bidirectionalOpposite;
+	private GraphMLEdgeType infoAggregation;
+//	private GraphMLEdgeType bidirectionalOpposite;
 
 	public CS2ASAnalysisTypeProvider(IGraph<CS2ASAnalysisNode> graph) {
 		
@@ -70,10 +82,40 @@ public class CS2ASAnalysisTypeProvider implements  IElementTypeProvider<CS2ASAna
 			elementTypes.add(infoNode);
 		}
 	
-		actionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
-		actionNode.setName("actionNode");
-		actionNode.setColor("#FF6600");
-		actionNode.setShape(ShapeType.ELLIPSE);
+		astElementActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		astElementActionNode.setName("AstOperation");
+		astElementActionNode.setColor("#FF0000");
+		astElementActionNode.setShape(ShapeType.ELLIPSE);
+		
+		cstElementActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		cstElementActionNode.setName("CstOperation");
+		cstElementActionNode.setColor("#00FF00");
+		cstElementActionNode.setShape(ShapeType.ELLIPSE);
+		
+		attributePropertyActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		attributePropertyActionNode.setName("attributePropertyAction");
+		attributePropertyActionNode.setColor("#0000EE");
+		attributePropertyActionNode.setShape(ShapeType.ELLIPSE);
+		
+		containmentPropertyActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		containmentPropertyActionNode.setName("containmentPropertyAction");
+		containmentPropertyActionNode.setColor("#FF6600");
+		containmentPropertyActionNode.setShape(ShapeType.ELLIPSE);
+		
+		nonContainmentPropertyActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		nonContainmentPropertyActionNode.setName("nonContainmentPropertyAction");
+		nonContainmentPropertyActionNode.setColor("#FFFF00");
+		nonContainmentPropertyActionNode.setShape(ShapeType.ELLIPSE);
+		
+		containmentNameResoPropertyActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		containmentNameResoPropertyActionNode.setName("containmentNameResoPropertyAction");
+		containmentNameResoPropertyActionNode.setColor("#FF6600");
+		containmentNameResoPropertyActionNode.setShape(ShapeType.HEXAGON);
+		
+		nonContainmentNameResoPropertyActionNode = GraphmltypesFactory.eINSTANCE.createGraphMLNodeType();
+		nonContainmentNameResoPropertyActionNode.setName("nonContainmentNameResoPropertyAction");
+		nonContainmentNameResoPropertyActionNode.setColor("#FFFF00");
+		nonContainmentNameResoPropertyActionNode.setShape(ShapeType.HEXAGON);
 		
 		actionInput = GraphmltypesFactory.eINSTANCE.createGraphMLEdgeType();
 		actionInput.setName("actionInput");
@@ -85,22 +127,28 @@ public class CS2ASAnalysisTypeProvider implements  IElementTypeProvider<CS2ASAna
 		actionOutput.setColor("#000000");
 		actionOutput.setLineStyle(EdgeLineStyle.LINE);
 		
-		infoInheritance = GraphmltypesFactory.eINSTANCE.createGraphMLEdgeType();
-		infoInheritance.setName("actionOutput");
-		infoInheritance.setColor("#000000");
-		infoInheritance.setLineStyle(EdgeLineStyle.DOTTED);
+		infoAggregation = GraphmltypesFactory.eINSTANCE.createGraphMLEdgeType();
+		infoAggregation.setName("infoAggregation");
+		infoAggregation.setColor("#000000");
+		infoAggregation.setLineStyle(EdgeLineStyle.DOTTED);
 		
-		bidirectionalOpposite = GraphmltypesFactory.eINSTANCE.createGraphMLEdgeType();
-		bidirectionalOpposite.setName("bidirectionalOpposite");
-		bidirectionalOpposite.setColor("#000000");
-		bidirectionalOpposite.setLineStyle(EdgeLineStyle.LINE);
-		bidirectionalOpposite.setFromStyle(EdgeEndStyle.STANDARD);
+//		bidirectionalOpposite = GraphmltypesFactory.eINSTANCE.createGraphMLEdgeType();
+//		bidirectionalOpposite.setName("bidirectionalOpposite");
+//		bidirectionalOpposite.setColor("#000000");
+//		bidirectionalOpposite.setLineStyle(EdgeLineStyle.LINE);
+//		bidirectionalOpposite.setFromStyle(EdgeEndStyle.STANDARD);
 		
-		elementTypes.add(bidirectionalOpposite);
-		elementTypes.add(actionNode);
+//		elementTypes.add(bidirectionalOpposite);
+		elementTypes.add(astElementActionNode);
+		elementTypes.add(cstElementActionNode);
+		elementTypes.add(attributePropertyActionNode);
+		elementTypes.add(containmentPropertyActionNode);
+		elementTypes.add(containmentNameResoPropertyActionNode);
+		elementTypes.add(nonContainmentPropertyActionNode);
+		elementTypes.add(nonContainmentNameResoPropertyActionNode);		
 		elementTypes.add(actionInput);
 		elementTypes.add(actionOutput);
-		elementTypes.add(infoInheritance);
+		elementTypes.add(infoAggregation);
 	}
 
 	@Override
@@ -113,7 +161,39 @@ public class CS2ASAnalysisTypeProvider implements  IElementTypeProvider<CS2ASAna
 		CS2ASAnalysisNode computation = node.getObject();
 		
 		if (computation instanceof ActionNode) {
-			return actionNode;
+			if (computation instanceof NameResoPropertyObj) {
+				// TODO
+//				Property prop = (Property) featureObj.getFeature();
+//				if (prop.isComposite()) {
+//					return astContainmentNameResoProperty;
+//				} else {
+//					return astNonContainmentNameResoProperty;
+//				}
+				return null;
+			} else {			
+				if (computation instanceof OperationAction) {
+					Operation operation = ((OperationAction)computation).getOperation();
+					if ("ast".equals(operation.getName()))
+						return astElementActionNode;
+					else if ("cst".equals(operation.getName())) 
+						return cstElementActionNode;
+					else
+						return null;
+				} if (computation instanceof ConstructorPartAction) {
+					Property prop = ((ConstructorPartAction) computation).getProperty();
+					if (prop.getOpposite() == null) { 	// FIXME how to know that a prop is Class attribute or an Association end ?
+														// This is not ideal, but seems reasonably working
+						return attributePropertyActionNode;
+					} else if (prop.isComposite()) {
+						return containmentPropertyActionNode;
+					} else {
+						return nonContainmentPropertyActionNode;
+					}
+				} else {
+					throw new IllegalStateException("Unexpected computation element");
+				}
+			}			
+			
 		} else if (computation instanceof InfoNode){
 			String pPackage = getContainingPackageName(computation.getReferredElement());
 			return packageName2InfoNodes.get(pPackage);
@@ -128,20 +208,21 @@ public class CS2ASAnalysisTypeProvider implements  IElementTypeProvider<CS2ASAna
 		// For the time being, an edge between ast operations are dotted
 		CS2ASAnalysisNode fromComp = edge.getFrom().getObject();
 		CS2ASAnalysisNode toComp = edge.getTo().getObject();
-		if (fromComp instanceof ActionNode) {
+		if (fromComp instanceof ActionNode
+			|| toComp instanceof ActionNode) {
 			return actionOutput;
-		} else if (toComp instanceof ActionNode) {
-			return actionInput;
-		} else if (fromComp instanceof ExtendedPropertyInfo &&
-				toComp instanceof ExtendedPropertyInfo) {
-			ExtendedPropertyInfo fromProp = (ExtendedPropertyInfo) fromComp;
-			ExtendedPropertyInfo toProp = (ExtendedPropertyInfo) toComp;
-			if (fromProp.getProperty() == toProp.getProperty()) {  // must bet aggregation/inheritance link
-				return infoInheritance;
+//		} else if (toComp instanceof ActionNode) {
+//			return actionInput;
+		} else if (fromComp instanceof PropertyRef &&
+				toComp instanceof PropertyRef) {
+			PropertyRef fromProp = (PropertyRef) fromComp;
+			PropertyRef toProp = (PropertyRef) toComp;
+			if (fromProp.getProperty() == toProp.getProperty()) {  // must be aggregation/inheritance link
+				return infoAggregation;
 			}
 		} else if (fromComp instanceof InfoNode &&
 					toComp instanceof InfoNode){
-			return infoInheritance;
+			return infoAggregation;
 		}
 		
 		// else default edge style
