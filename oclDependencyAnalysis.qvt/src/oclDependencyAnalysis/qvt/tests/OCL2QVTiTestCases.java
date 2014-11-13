aa13075c49587d2ea94e44eccacce0c2eb7756ed
Adolfo SBH <adolfosbh@gmail.com>
2014-11-13 13:54:35 +0000

    - Uploading some execution infrastructure from java code

--- oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/OCL2QVTiTestCases.java
+++ oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/OCL2QVTiTestCases.java
@@ -1,8 +1,6 @@
 package oclDependencyAnalysis.qvt.tests;
 
-import static org.junit.Assert.*;
-
-import java.net.URL;
+import java.net.URISyntaxException;
 
 import oclDependencyAnalysis.qvt.OCL2QVTiBroker;
 
@@ -12,7 +10,11 @@ import org.eclipse.ocl.examples.domain.utilities.DomainUtil;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter;
 import org.eclipse.ocl.examples.pivot.model.OCLstdlib;
+import org.eclipse.ocl.examples.xtext.completeocl.CompleteOCLStandaloneSetup;
+import org.eclipse.qvtd.build.etl.EtlTask;
 import org.eclipse.qvtd.build.etl.MtcBroker;
+import org.eclipse.qvtd.build.etl.PivotModel;
+import org.eclipse.qvtd.build.etl.QvtMtcExecutionException;
 import org.eclipse.qvtd.pivot.qvtcore.QVTcorePivotStandaloneSetup;
 import org.eclipse.qvtd.pivot.qvtimperative.QVTimperativePivotStandaloneSetup;
 import org.junit.Before;
@@ -20,21 +22,40 @@ import org.junit.Test;
 
 public class OCL2QVTiTestCases {
 
+	protected static final String ECORE_URI = "http://www.eclipse.org/emf/2002/Ecore";
+	
+	protected static final String PIVOT_URI = "http://www.eclipse.org/ocl/3.1.0/Pivot";
+	
+	protected static final String QVTB_URI ="http://www.eclipse.org/qvt/0.9/QVTbase";
+	
+	protected static final String QVTCB_URI = "http://www.eclipse.org/qvt/0.9/QVTcoreBase";
+	
+	protected static final String QVTC_URI = "http://www.eclipse.org/qvt/0.9/QVTcore";
+	
+	protected static final String QVTI_URI = "http://www.eclipse.org/qvt/0.9/QVTimperative";
+		
+	protected static final String QVTI_FULL_NS = QVTI_URI + ',' + QVTCB_URI + ',' +  QVTB_URI + ',' +  PIVOT_URI;
+	
+	protected static final String TRACES_FULL_NS = PIVOT_URI + ',' + ECORE_URI;
+	
 	private MetaModelManager metaModelManager; 
 	private ResourceSet rSet;
+	private PivotModelUtil pmUtil;
 	
 	@Before
 	public void setUp() throws Exception {
+		OCLstdlib.install();
 		QVTcorePivotStandaloneSetup.doSetup();
 		QVTimperativePivotStandaloneSetup.doSetup();
-		OCLstdlib.install();
+		CompleteOCLStandaloneSetup.doSetup();
+		
 				
 		rSet = new ResourceSetImpl();
 		metaModelManager = new MetaModelManager();
+		pmUtil = new PivotModelUtil(metaModelManager);
         MetaModelManagerResourceSetAdapter.getAdapter(DomainUtil.nonNullState(rSet), metaModelManager);
 	}
 
-	@Test
 	public void test() throws Exception {
 				
 		String qvtcasUri = "platform:/resource/oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/models/example2/classescs2as.qvtc"; 
@@ -44,5 +65,37 @@ public class OCL2QVTiTestCases {
     	
     	System.out.println("finished!!!");
 	}
+		
+	@Test
+	public void testOCL2QVTi_classesExample() throws Exception {
+				
+		String oclDocURI = "platform:/resource/oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/models/example2/ClassesCS2Classes.ocl.oclas";
+		
+		String qvtiFileURI = "platform:/resource/oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/models/example2/classescs2as.output.qvtp.qvtias";
+		
+		String tracesMMURI = "platform:/resource/oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/models/example2/ClassesCS2ClassesAS.ecore.oclas";
+		
+		runOCL2QVTi(oclDocURI, qvtiFileURI, tracesMMURI);
+		
+		// Testing nothing for the time being. Just to be able to debug java code.
+	}
+	
+	@Test
+	protected void runOCL2QVTi (String oclDocURI, String qvtiFileURI, String tracesMMURI) throws QvtMtcExecutionException, URISyntaxException {
 
+		String oclDocModelName = "OCL";
+		PivotModel oclModel = pmUtil.createPivotModel(oclDocURI, oclDocModelName, "", PIVOT_URI, true, false, true, true);
+		
+		String qvtiModelName = "QVTi";
+		PivotModel qvtiModel = pmUtil.createPivotModel(qvtiFileURI, qvtiModelName, "", QVTI_FULL_NS, false, true, false, true);
+		
+		String tracesMModelName = "MiddleMM";
+		PivotModel tracesMM = pmUtil.createPivotModel(tracesMMURI, tracesMModelName, "", TRACES_FULL_NS , true, false, true, true);
+		
+		EtlTask eol = new EtlTask(OCL2QVTiTestCases.class.getResource("/oclDependencyAnalysis/qvt/ocl2qvti.etl").toURI());
+		eol.addModel(oclModel);
+		eol.addModel(qvtiModel);
+		eol.addModel(tracesMM);
+		eol.execute();
+	}
 }
