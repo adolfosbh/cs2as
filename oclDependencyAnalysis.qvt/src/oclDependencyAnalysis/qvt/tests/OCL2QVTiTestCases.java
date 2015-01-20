e6323b90dcb9955858be3e504da982bfbfa0f485
Adolfo SBH <adolfosbh@gmail.com>
2015-01-20 17:01:50 +0000

    - Activating modelRef comparison in the Toy example - Adding disambiguating predicates to the output creation rules

--- oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/OCL2QVTiTestCases.java
+++ oclDependencyAnalysis.qvt/src/oclDependencyAnalysis/qvt/tests/OCL2QVTiTestCases.java
@@ -114,7 +114,7 @@ public class OCL2QVTiTestCases extends LoadTestCase {
 		URI samplesBaseUri = baseURI.appendSegment("samples");
     	URI csModelURI = samplesBaseUri.appendSegment("model1_input.xmi");
     	URI asModelURI = samplesBaseUri.appendSegment("model1_output.xmi");
-//    	URI expectedAsModelURI = samplesBaseUri.appendSegment("example1_output_expected.xmi");
+    	URI expectedAsModelURI = samplesBaseUri.appendSegment("model1_output_ref.xmi");
     	
     	testEvaluator.saveTransformation(null);
         testEvaluator.loadModel("leftCS", csModelURI);
@@ -123,9 +123,9 @@ public class OCL2QVTiTestCases extends LoadTestCase {
         testEvaluator.saveModels();
         testEvaluator.dispose();
                 
-//        ResourceSet rSet = metamodelManager.getExternalResourceSet();
-//        Resource expected =  rSet.getResource(expectedAsModelURI, true);
-//        assertSameModel(expected, rSet.getResource(asModelURI, true));
+        ResourceSet rSet = metamodelManager.getExternalResourceSet();
+        Resource expected =  rSet.getResource(expectedAsModelURI, true);
+        assertSameModel(expected, rSet.getResource(asModelURI, true));
 	}
 	
 	
